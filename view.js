ef281730d6f7bba5c49905e6bd8995d0b4522acb
cppizarro <cppizarro@miuandes.cl>
2021-05-12 22:52:08 -0400

    avances view.js

---
+++ view.js
@@ -0,0 +1,75 @@
+const figlet = require('figlet')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+//const { get } = require('node:http')
+
+function getTitle(){
+    return chalk.green(
+        figlet.textSync(
+            'App',
+            {
+                horizontalLayout: 'default',
+                font: 'Nancyj-Underlined'
+            }
+        )
+    )
+}
+
+/*
+function getTable(model){
+    const {counter} = model
+    return [
+        {Counter: counter},
+    ]
+}
+
+function inputForm(model){
+    const {input} = model
+    const message = 'Increase or decrease?'
+    return inquirer.prompt([
+        {
+            name: 'input',
+            type: 'input',
+            message: message,
+            default: input,
+            validate: function(value){
+                if(value === '+' || value === '-'){
+                    return true
+                } else {
+                    return 'Enter + or -'
+                }
+            }
+        }
+    ])
+}
+
+function listForm(model){
+    const {input} = model
+    const message = 'Increase or decrease?'
+    const choices = ['+', '-']
+    return inquirer.prompt({
+        name: 'input',
+        type: 'list',
+        message: message,
+        default: input,
+        choices: choices
+    })
+}
+
+// Get actual console view
+function view(model){
+    return {
+        title: getTitle(),
+        table: getTable(model)
+    }
+}
+
+module.exports = {
+    view, 
+    inputForm,
+    listForm
+}
+*/
+
+title = getTitle()
+console.log(title)
